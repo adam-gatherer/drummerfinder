000f71ebf88d0ace8e6c45644b3f4827416a5071
Adam G <86427962+cap-blackett@users.noreply.github.com>
2022-07-11 20:40:10 +0100

    Updated script after review
    Changelog after review by Jakub L.
    
    page_2_maker:
    - Renamed to generate_pg_2
    - Renamed variables (regions, new_regions)
    - Renamed loop variables (region)
    
    ad_scraper_v2:
    - Rnamed to scrape_ads
    - Removed redundant ad_list
    - Renamed loop variables (ad)
    - Renamed date_class to date_tag for clarification
    - Changed ad_dict values from lists to tuples
    
    main:
    - Broke region splitting off into new function (split_regions)
    - Renamed variables to remove variable type from name
    - Renamed loop variables
    - You probably want to break up the function you're running on each region into a separate function
    - Removed unnecessary parentheses
    
    split_regions:
    - Created function out of repeated section in main()
    - Changed methodology from multiple lists to dictionary of lists (locations_by_region)
    
    General:
    - Changed 'import urllib' to 'from urllib.request import urlopen' for neatness
    - Addded comments to regexes for clarification
    - Tidied up comments, left 'teehee' at start of file because I'm a cheeky chappy

--- main.py
+++ main.py
@@ -1,105 +1,112 @@
 # script to scrape joinmyband for drummers teehee
-import urllib.request
+from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
 
 
-# unused, depreciated, hated, reviled
-def ad_scraper(url):
-    drum_ad_list = []
+def scrape_ads(url):
     # url = 'https://www.joinmyband.co.uk/classifieds/scotland-east-f20.html'
-    html_page = urllib.request.urlopen(url)
+    html_page = urlopen(url)
     soup = BeautifulSoup(html_page, 'html.parser')
-    ad_list = (soup.find_all('a'))
-    for a in ad_list:
-        if 'drum' in str(a):
-            a = (re.search('"(.*)"', str(a)).group(1))
-            if 'classifieds' in a:
-                a = a[13:]
-            a = f'https://www.joinmyband.co.uk/classifieds/{a}'
-            drum_ad_list.append(a)
-    return drum_ad_list
-
-
-def ad_scraper_v2(url):
-    # url = 'https://www.joinmyband.co.uk/classifieds/scotland-east-f20.html'
-    html_page = urllib.request.urlopen(url)
-    soup = BeautifulSoup(html_page, 'html.parser')
-    # ads = soup.find_all("p", {"class": "title"})
     ads = soup.find_all("div", {"class": re.compile('topic-row row.')})
-    ad_list = []
     ad_dict = {}
-    for a in ads:
-        title = (re.search('(html"\>.*\<\/a)', str(a)).group(1))[6:-3]
-        date_class = a.find("p", {"class": "small"})
-        date = ((re.search('(small"\>.*by)', str(date_class)).group(1))[7:-3])
-        location = (re.search('(\<span\>\(.*\)\<\/span\>)', str(a)).group(1)[6:-7])
-        link = f"https://www.joinmyband.co.uk/classifieds/{((re.search('(href=.*html)', str(a)).group(1))[6:])}"
-        # ad_list.append(link)
-        ad_dict[link] = [title, location, date]
+    for ad in ads:
+        # Finds ad title via <a href> tag, chops tags off start/end
+        title = (re.search('(html"\>.*\<\/a)', str(ad)).group(1))[6:-3]
+        date_tag = ad.find("p", {"class": "small"})
+        # Finds ad date via small class, chops off end of opening <p> tag and removes "by {username}"
+        date = ((re.search('(small"\>.*by)', str(date_tag)).group(1))[7:-3])
+        # Finds location by searching for contents in parenthesis () between <span> tag, only one per ad
+        location = (re.search('(\<span\>\(.*\)\<\/span\>)', str(ad)).group(1)[6:-7])
+        link = f"https://www.joinmyband.co.uk/classifieds/{((re.search('(href=.*html)', str(ad)).group(1))[6:])}"
+        ad_dict[link] = (title, location, date)
     return ad_dict
 
 
-def page_2_maker(region_list):
-    new_region_list = []
-    for a in region_list:
+def generate_pg_2(regions):
+    new_regions = []
+    for region in regions:
         pg2 = '-s25.html'
-        new_region_list.append(a)
-        new_region_list.append(a[0:-5] + pg2)
-    return new_region_list
+        new_regions.append(region)
+        new_regions.append(region[0:-5] + pg2)
+    return new_regions
+
+
+def split_regions(regions):
+    scotland_regions = []
+    ireland_regions = []
+    wales_regions = []
+    england_regions = []
+    for region in regions:
+        if 'scotland' in region:
+            scotland_regions.append(region)
+        elif 'ireland' in region:
+            ireland_regions.append(region)
+        elif 'wales' in region:
+            wales_regions.append(region)
+        elif 'national' in region:
+            None
+        else:
+            england_regions.append(region)
+    locations_by_region = {
+        "scotland": scotland_regions,
+        "ireland": ireland_regions,
+        "wales": wales_regions,
+        "england": england_regions,
+    }
+    return locations_by_region
 
 
 def main():
     #
-    # probably gonna break this up, there's a LOT going on in main
+    # Probably gonna break this up, there's a LOT going on in main
     #
     site = 'https://www.joinmyband.co.uk/'
-    html_page = urllib.request.urlopen(site)
+    html_page = urlopen(site)
     soup = BeautifulSoup(html_page, 'html.parser')
-    region_list = []
-    ad_dicts_list = []
-    # find the unordered list of links classed as "regions"
+    all_regions = []
+    # Find the unordered list of links classed as "regions"
     region_links = soup.find_all("ul", class_="regions")
-    # makes bs object 'regions' into a string, splits it by line to a list, chops off the <ul> and </ul> at start/end,
-    # then cycles through the list items to strip the html part from the url to use later
-    for a in (str(region_links[0]).split("\n"))[1:-1]:
-        a = (re.search('(classifieds\/.*\.html)', str(a)).group(1))
-        region_list.append(a)
-    # split the countries
-    region_list_scot = []
-    region_list_ire = []
-    region_list_wales = []
-    region_list_engl = []
-    for a in region_list:
-        if 'scotland' in a:
-            region_list_scot.append(a)
-        elif 'ireland' in a:
-            region_list_ire.append(a)
-        elif 'wales' in a:
-            region_list_wales.append(a)
-        elif 'national' in a:
-            None
-        else:
-            region_list_engl.append(a)
-    region_list_scot = page_2_maker(region_list_scot)
-    # cycle through regions in list and run ad_scraper on the ad page for said region
-    for a in region_list_scot:
-        if 's25' not in a:
-            region_name = ((re.match('(.*-)', (a[12:])).group(1))[0:-1])
-            print(10 * "=" + region_name.upper() + 10 * "=" + "\n")
-        url = site + a
-        a_dict = (ad_scraper_v2(url))
+    # Makes bs object 'regions' into a string, splits it by line to a list, chops off the <ul> and </ul> at start/end,
+    # Then cycles through the list items to strip the html part from the url to use later
+    for list_item in (str(region_links[0]).split("\n"))[1:-1]:
+        # Finds the page address for each region by matching the start of the href target
+        list_item = re.search('(classifieds\/.*\.html)', str(list_item)).group(1)
+        all_regions.append(list_item)
+    # Adds second page links to region page links
+    all_regions = generate_pg_2(all_regions)
+    # Splits all_regions into lists in the dictionary locations_by_region
+    locations_by_region = split_regions(all_regions)
+    for region in locations_by_region["scotland"]:
+        #
+        # old code, made redundant by generate_pg_2()
+        #
+        # if 's25' not in region:
+        #    region_name = ((re.match('(.*-)', (region[12:])).group(1))[0:-1])
+        # print(10 * "=" + region_name.upper() + 10 * "=" + "\n")
+        url = site + region
+        a_dict = (scrape_ads(url))
         for key, val in a_dict.items():
             if 'drum' in str(key).lower():
                 None
-                print(f'{val[0]}\n{val[1]}\n{val[2]}\n{key}\n')
-        print("\n")
+                # print(f'{val[0]}\n{val[1]}\n{val[2]}\n{key}\n')
+        # print("\n")
 
-    counties = []
-    for a in region_list_engl:
-        # snips the full html page name thinger down to just the county by running a regex match for anything up to the
-        # last -, then chopping the - off the end. Match is run against the string excluding the classifieds part
-        counties.append((re.match('(.*-)', (a[12:])).group(1))[0:-1])
+    # for key, val in locations_by_region.items():
+    #     if "england" not in key:
+    #         print(key)
+    #         for region in val:
+    #             print(region)
+    #
+    # Not sure how I'll use this, but England is unmanageable without further division. May go for North/South divide.
+    #
+    # counties = []
+    # for a in locations_by_region["england"]:
+    #     # snips the full html page name thinger down to just the county by running a regex match for anything up to
+    #     # the last -, then chopping the - off the end. Match is run against the string excluding the classifieds part
+    #     trunc = (re.search('(.*-f)', (a[12:])).group(1))[0:-2]
+    #     if trunc not in counties:
+    #        counties.append(trunc)
 
 
 if __name__ == "__main__":
